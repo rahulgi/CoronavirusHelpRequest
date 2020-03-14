74cffb35554c40f881040b35bb6be098416fd058
Rahul Gupta-Iwasaki <rahul@every.org>
2020-03-14 15:21:47 -0700

    Add quote.

--- web_app/src/components/pages/Browse.tsx
+++ web_app/src/components/pages/Browse.tsx
@@ -2,5 +2,12 @@ import React from "react";
 import { DefaultLayout } from "../common/DefaultLayout";
 
 export const BrowsePage: React.FC = () => {
-  return <DefaultLayout pageTitle="Find requests"></DefaultLayout>;
+  return (
+    <DefaultLayout pageTitle="Find requests">
+      <p>
+        Ask not what your country can do for you — ask what you can do for your
+        country. - John F. Kennedy
+      </p>
+    </DefaultLayout>
+  );
 };
