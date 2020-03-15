17ececb05c31fa2e822c87e3147b3782d0c0acbf
Rahul Gupta-Iwasaki <rahul@every.org>
2020-03-14 19:57:24 -0700

    Remove logged in status from homepage.

--- web_app/src/components/pages/Browse.tsx
+++ web_app/src/components/pages/Browse.tsx
@@ -2,18 +2,14 @@ import React from "react";
 
 import { DefaultLayout } from "../common/DefaultLayout";
 import { HelpRequestsList } from "../HelpRequestsList";
-import { useLoggedInStatus } from "../contexts/AuthContext";
 
 export const BrowsePage: React.FC = () => {
-  const loggedInStatus = useLoggedInStatus();
-
   return (
     <DefaultLayout pageTitle="Browse help requests">
       <p>
         &ldquo;Ask not what your country can do for you — ask what you can do
         for your country.&rdquo; - John F. Kennedy
       </p>
-      <p>You are {loggedInStatus}</p>
       <HelpRequestsList />
     </DefaultLayout>
   );
