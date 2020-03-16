9d10e142ee02c3c5a378e1a1c8f975bef19b5f2e
Rahul Gupta-Iwasaki <rahul@every.org>
2020-03-16 16:54:24 -0700

    ThreadCard styling.

--- web_app/src/components/ThreadCard.tsx
+++ web_app/src/components/ThreadCard.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import TimeAgo from "react-timeago";
 import styled from "@emotion/styled/macro";
 import { Link } from "react-router-dom";
 
 import { Thread } from "../firebase/storage/messaging";
-import { Card } from "./common/Material/Card";
+import {
+  Card,
+  CardPrimaryAction,
+  CardOverline,
+  CardTitle,
+  CardSubtitle
+} from "./common/Material/Card";
+import { UserChip } from "./common/UserChip";
+import { useCurrentUserId } from "./contexts/AuthContext";
+import { useHelpRequest } from "../hooks/data/useHelpRequest";
+import { HelpRequestStatusChip } from "./common/HelpRequestStatusChip";
 
 const StyledLink = styled(Link)`
   color: inherit;
@@ -15,13 +26,30 @@ interface ThreadCardProps {
 }
 
 export const ThreadCard: React.FC<ThreadCardProps> = ({ thread }) => {
-  const { helpRequestId, lastMessageAt } = thread;
+  const { helpRequestId, lastMessageAt, participantIds } = thread;
+  const currentUserId = useCurrentUserId();
+  const recipientId = participantIds.filter(id => id !== currentUserId)[0];
+  const helpRequestResult = useHelpRequest(helpRequestId);
 
   return (
     <StyledLink to={`request/${helpRequestId}/messages`}>
       <Card>
-        <h2>Thread with TODO</h2>
-        <p>Last message at: {lastMessageAt.toLocaleString()}</p>
+        <CardPrimaryAction>
+          <CardOverline>
+            {recipientId && <UserChip userId={recipientId} />}
+          </CardOverline>
+          <CardTitle>
+            {helpRequestResult.result && helpRequestResult.result.title}
+          </CardTitle>
+          <CardSubtitle>
+            {helpRequestResult.result && (
+              <HelpRequestStatusChip status={helpRequestResult.result.status} />
+            )}
+            <span>
+              Last message: <TimeAgo date={lastMessageAt} />
+            </span>
+          </CardSubtitle>
+        </CardPrimaryAction>
       </Card>
     </StyledLink>
   );
