c5606f6560e9c9dbf1c43ffe406b3d69ab97ee58
Rahul Gupta-Iwasaki <rahul@every.org>
2020-03-17 01:27:40 -0700

    Styling.

--- web_app/src/components/pages/About.tsx
+++ web_app/src/components/pages/About.tsx
@@ -6,6 +6,7 @@ import { PALETTE } from "../../styles/colors";
 import { HelpRequestStatusChip } from "../common/HelpRequestStatusChip";
 import { HelpRequestStatus } from "../../firebase/storage/helpRequest";
 import { Card, CardBody, CardBodyText } from "../common/Material/Card";
+import { spacing } from "../../styles/spacing";
 
 const HelpRequest = styled.span`
   color: ${PALETTE.error};
@@ -19,63 +20,76 @@ const Warning = styled.span`
   color: ${PALETTE.error};
 `;
 
+const Section = styled.section`
+  & > *:not(:last-child) {
+    margin-bottom: ${spacing.s};
+  }
+`;
+
 export const AboutPage: React.FC = () => {
   return (
     <DefaultLayout pageTitle="About">
-      <h5>What is it?</h5>
-      <p>
-        <b>Coronavirus Help Requests</b> was built to help people within
-        communities help each other in the face of the Covid19 (Coronavirus)
-        Pandemic.
-      </p>
-      <Card>
-        <CardBody>
-          <CardBodyText>
-            <Warning>
-              This site is not intended to solve urgent health-related requests.
-              If you're having an emergency, please contact 911 (or your local
-              equivalent).
-            </Warning>
-          </CardBodyText>
-        </CardBody>
-      </Card>
-      <h5>How does it work?</h5>
-      <p>
-        Vulnerable community members post{" "}
-        <HelpRequest>Help Requests</HelpRequest> with their general location and
-        what they need.{" "}
-      </p>
-      <p>
-        Healthy community members post <HelpOffer>Help Offers</HelpOffer> with
-        their location and the distance they can travel. Anyone who has posted a{" "}
-        <HelpOffer>Help Offer</HelpOffer> will also get notified when a new{" "}
-        <HelpRequest>Help Request</HelpRequest> is made within the range of
-        their offer.
-      </p>
-      <p>
-        Helpers can send messages to the creator of{" "}
-        <HelpRequest>Help Requests</HelpRequest> to figure out any details they
-        need to complete the request.
-      </p>
-      <p>
-        When a <HelpRequest>Help Request</HelpRequest> is completed, the
-        requester marks it as{" "}
-        <HelpRequestStatusChip status={HelpRequestStatus.RESOLVED} /> to
-        indicate that they no longer need help with that request.
-      </p>
-      <h5>Who built this?</h5>
-      <p>
-        <b>Coronavirus Help Requests</b> was built by{" "}
-        <a href="https://rahulgi.com" target="_blank">
-          Rahul Gupta-Iwasaki
-        </a>
-        . He currently works as the Head of Product at{" "}
-        <a href="https://every.org" target="_blank">
-          Every.org
-        </a>{" "}
-        and lives in San Francisco. If you have any questions/comments/concerns
-        about this site, please email him at rahul.guptaiwasaki(at)gmail.com.
-      </p>
+      <Section>
+        <h5>What is it?</h5>
+        <p>
+          <b>Coronavirus Help Requests</b> was built to help people within
+          communities help each other in the face of the Covid19 (Coronavirus)
+          Pandemic.
+        </p>
+        <Card>
+          <CardBody>
+            <CardBodyText>
+              <Warning>
+                This site is not intended to solve urgent health-related
+                requests. If you're having an emergency, please contact 911 (or
+                your local equivalent).
+              </Warning>
+            </CardBodyText>
+          </CardBody>
+        </Card>
+      </Section>
+      <Section>
+        <h5>How does it work?</h5>
+        <p>
+          Vulnerable community members post{" "}
+          <HelpRequest>Help Requests</HelpRequest> with their general location
+          and what they need.{" "}
+        </p>
+        <p>
+          Healthy community members post <HelpOffer>Help Offers</HelpOffer> with
+          their location and the distance they can travel. Anyone who has posted
+          a <HelpOffer>Help Offer</HelpOffer> will also get notified when a new{" "}
+          <HelpRequest>Help Request</HelpRequest> is made within the range of
+          their offer.
+        </p>
+        <p>
+          Helpers can send messages to the creator of{" "}
+          <HelpRequest>Help Requests</HelpRequest> to figure out any details
+          they need to complete the request.
+        </p>
+        <p>
+          When a <HelpRequest>Help Request</HelpRequest> is completed, the
+          requester marks it as{" "}
+          <HelpRequestStatusChip status={HelpRequestStatus.RESOLVED} /> to
+          indicate that they no longer need help with that request.
+        </p>
+      </Section>
+      <Section>
+        <h5>Who built this?</h5>
+        <p>
+          <b>Coronavirus Help Requests</b> was built by{" "}
+          <a href="https://rahulgi.com" target="_blank">
+            Rahul Gupta-Iwasaki
+          </a>
+          . He currently works at{" "}
+          <a href="https://every.org" target="_blank">
+            Every.org
+          </a>{" "}
+          and lives in San Francisco. If you have any
+          questions/comments/concerns about this site, please email him at
+          rahul.guptaiwasaki(at)gmail.com.
+        </p>
+      </Section>
     </DefaultLayout>
   );
 };
--- web_app/src/styles/text.ts
+++ web_app/src/styles/text.ts
@@ -3,4 +3,8 @@ import { materialTextStyles } from "./materialTypography";
 
 export const textStyles = css`
   ${materialTextStyles}
+
+  h1, h2, h3, h4, h5, h6, p {
+    margin: 0;
+  }
 `;
