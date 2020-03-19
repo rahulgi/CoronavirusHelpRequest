fefaaa16e10cd1b8b3e673ba8f116cf5e861dd4a
Rahul Gupta-Iwasaki <rahul@every.org>
2020-03-18 23:29:34 -0700

    Removed key information.

--- web_app/src/config.ts
+++ web_app/src/config.ts
@@ -1,13 +1,6 @@
 // Retrieved from the Firebase Console -> Settings -> General -> Config
 export const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyCDQkDeYC3TXbJtG4lM6okHS-zN3xHps5E",
-  authDomain: "coronavirushelprequest-2f3f9.firebaseapp.com",
-  databaseURL: "https://coronavirushelprequest-2f3f9.firebaseio.com",
-  projectId: "coronavirushelprequest-2f3f9",
-  storageBucket: "coronavirushelprequest-2f3f9.appspot.com",
-  messagingSenderId: "561662606033",
-  appId: "1:561662606033:web:4b4d7cc98fe61f4d84e3f0",
-  measurementId: "G-PV7WY3V78S"
+  // COPY-CONFIG-HERE
 };
 
-export const GOOGLE_MAPS_API_KEY = "AIzaSyCDQkDeYC3TXbJtG4lM6okHS-zN3xHps5E";
+export const GOOGLE_MAPS_API_KEY = "COPY-KEY-HERE";
